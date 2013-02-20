7e63dce481f9d4c0846be82d803df67315410735
David Prater <dprater@cisco.com>
2013-02-20 13:54:15 -0500

    Added ability to add a prefix and/or suffix to the token generated.

--- lib/mongoid_token.rb
+++ lib/mongoid_token.rb
@@ -11,6 +11,8 @@ module Mongoid
         options[:retry] ||= 3
         options[:contains] ||= :alphanumeric
         options[:field_name] ||= :token
+        options[:prefix] ||= ''
+        options[:suffix] ||= ''
         #options[:key] ||= false
 
         self.field options[:field_name].to_sym, :type => String
@@ -21,11 +23,13 @@ module Mongoid
         #end
 
         set_callback(:create, :before) do |document|
-          document.create_token(options[:length], options[:contains])
+          document.create_token(options[:length], options[:contains], 
+                                options[:prefix], options[:suffix])
         end
 
         set_callback(:save, :before) do |document|
-          document.create_token_if_nil(options[:length], options[:contains])
+          document.create_token_if_nil(options[:length], options[:contains],
+                                       options[:prefix], options[:suffix])
         end
 
         after_initialize do # set_callback did not work with after_initialize callback
@@ -33,6 +37,8 @@ module Mongoid
           self.instance_variable_set :@token_field_name, options[:field_name]
           self.instance_variable_set :@token_length, options[:length]
           self.instance_variable_set :@token_contains, options[:contains]
+          self.instance_variable_set :@token_prefix, options[:prefix]
+          self.instance_variable_set :@token_suffix, options[:suffix]
         end
 
         if options[:retry] > 0
@@ -67,7 +73,7 @@ module Mongoid
                           e.details['err'] =~ /"#{self.send(@token_field_name.to_sym)}"/
 
         if (retries -= 1) > 0
-          self.create_token(@token_length, @token_contains)
+          self.create_token(@token_length, @token_contains, @token_prefix, @token_suffix)
           retry
         else
           Rails.logger.warn "[Mongoid::Token] Warning: Maximum to generation retries (#{@max_collision_retries}) exceeded." if defined?(Rails) && Rails.env == 'development'
@@ -84,33 +90,36 @@ module Mongoid
       resolve_token_collisions { with(:safe => true).upsert_without_safety(options) }
     end
 
-    def create_token(length, characters)
-      self.send(:"#{@token_field_name}=", self.generate_token(length, characters))
+    def create_token(length, characters, prefix, suffix)
+      self.send(:"#{@token_field_name}=", self.generate_token(length, characters, prefix, suffix))
     end
 
-    def create_token_if_nil(length, characters)
+    def create_token_if_nil(length, characters, prefix, suffix)
       if self[@token_field_name.to_sym].blank?
-        self.create_token(length, characters) 
+        self.create_token(length, characters, prefix, suffix) 
       end
     end
 
-    def generate_token(length, characters = :alphanumeric)
+    def generate_token(length, characters = :alphanumeric, prefix = '', suffix = '')
+      tok = ''
       case characters
       when :alphanumeric
-        (1..length).collect { (i = Kernel.rand(62); i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr }.join
+        tok = (1..length).collect { (i = Kernel.rand(62); i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr }.join
       when :numeric
-        rand(10**length).to_s
+        tok = rand(10**length).to_s
       when :fixed_numeric
-        rand(10**length).to_s.rjust(length,rand(10).to_s)
+        tok = rand(10**length).to_s.rjust(length,rand(10).to_s)
       when :fixed_numeric_no_leading_zeros
-        (rand(10**length - 10**(length-1)) + 10**(length-1)).to_s
+        tok = (rand(10**length - 10**(length-1)) + 10**(length-1)).to_s
       when :alpha
-        Array.new(length).map{['A'..'Z','a'..'z'].map{|r|r.to_a}.flatten[rand(52)]}.join
+        tok = Array.new(length).map{['A'..'Z','a'..'z'].map{|r|r.to_a}.flatten[rand(52)]}.join
       when :alpha_lower
-        Array.new(length).map{['a'..'z'].map{|r|r.to_a}.flatten[rand(26)]}.join
+        tok = Array.new(length).map{['a'..'z'].map{|r|r.to_a}.flatten[rand(26)]}.join
       when :alpha_upper
-        Array.new(length).map{['A'..'Z'].map{|r|r.to_a}.flatten[rand(26)]}.join
+        tok = Array.new(length).map{['A'..'Z'].map{|r|r.to_a}.flatten[rand(26)]}.join
       end
+
+      prefix + tok + suffix
     end
   end
 end
