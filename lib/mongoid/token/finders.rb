379dc2fdff047c6cbe770f12c2525cd5f02e44e5
Nicholas Bruning <nicholas@bruning.com.au>
2014-07-12 15:11:48 +1000

    Added support for finding by multiple tokens
    Closes #39

--- lib/mongoid/token/finders.rb
+++ lib/mongoid/token/finders.rb
@@ -2,8 +2,12 @@ module Mongoid
   module Token
     module Finders
       def self.define_custom_token_finder_for(klass, field_name = :token)
-        klass.define_singleton_method(:"find_by_#{field_name.to_s}") do |token|
-          self.find_by(field_name.to_sym => token)
+        klass.define_singleton_method(:"find_by_#{field_name}") do |token|
+          if token.is_a?(Array)
+            self.in field_name.to_sym => token
+          else
+            self.find_by field_name.to_sym => token
+          end
         end
 
         klass.define_singleton_method :"find_with_#{field_name}" do |*args| # this is going to be painful if tokens happen to look like legal object ids
--- spec/mongoid/token/finders_spec.rb
+++ spec/mongoid/token/finders_spec.rb
@@ -1,9 +1,11 @@
 require File.join(File.dirname(__FILE__), %w[.. .. spec_helper])
 
 describe Mongoid::Token::Finders do
-  before :each do
+  after do
+    Object.send(:remove_const, :Document) if Object.constants.include?(:Document)
+    Object.send(:remove_const, :AnotherDocument) if Object.constants.include?(:AnotherDocument)
   end
-
+  
   it "define a finder based on a field_name" do
     klass = Class.new
     field = :another_token
@@ -32,10 +34,26 @@ describe Mongoid::Token::Finders do
     Document.find_by_token("1234").should == document
   end
 
+  it 'retrieves multiple documents using the dynamic finder' do
+    class Document; include Mongoid::Document; field :token; end
+    document = Document.create!(:token => "1234")
+    document2 = Document.create!(:token => "5678")
+    Mongoid::Token::Finders.define_custom_token_finder_for(Document)
+    Document.find_by_token(["1234", "5678"]).should == [document, document2]
+  end
+
   it "retrieve a document using the `find` method" do
     class AnotherDocument; include Mongoid::Document; field :token; end
     document = AnotherDocument.create! :token => "1234"
     Mongoid::Token::Finders.define_custom_token_finder_for(AnotherDocument)
     AnotherDocument.find("1234").should == document
+  end
+
+  it 'retrieves multiple documents using the `find` method' do
+    class AnotherDocument; include Mongoid::Document; field :token; end
+    document = AnotherDocument.create! :token => "1234"
+    document2 = AnotherDocument.create! :token => "5678"
+    Mongoid::Token::Finders.define_custom_token_finder_for(AnotherDocument)
+    AnotherDocument.find(["1234", "5678"]).should == [document, document2]
   end
 end
