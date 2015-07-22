f977ac60dc310847f94ac3ff6488effe9c7a4aa1
Nicholas Bruning <nicholas@bruning.com.au>
2015-07-22 10:34:44 +1000

    Added failing test case for using `find' on embedded docs
    Refer #52

--- spec/mongoid/token/finders_spec.rb
+++ spec/mongoid/token/finders_spec.rb
@@ -4,8 +4,9 @@ describe Mongoid::Token::Finders do
   after do
     Object.send(:remove_const, :Document) if Object.constants.include?(:Document)
     Object.send(:remove_const, :AnotherDocument) if Object.constants.include?(:AnotherDocument)
+    Object.send(:remove_const, :EmbeddedDocument) if Object.constants.include?(:EmbeddedDocument)
   end
-  
+
   it "define a finder based on a field_name" do
     klass = Class.new
     field = :another_token
@@ -55,5 +56,27 @@ describe Mongoid::Token::Finders do
     document2 = AnotherDocument.create! :token => "5678"
     Mongoid::Token::Finders.define_custom_token_finder_for(AnotherDocument)
     AnotherDocument.find(["1234", "5678"]).should == [document, document2]
+  end
+
+  it 'retrieves embedded documents using the dynamic finder' do
+    class Document; include Mongoid::Document; field :token; embeds_many :embedded_documents; end
+    class EmbeddedDocument; include Mongoid::Document; field :token; embedded_in :document; end
+
+    document = Document.create!(:token => "1234")
+    embedded_document = EmbeddedDocument.create(token: "5678", document: document)
+
+    Mongoid::Token::Finders.define_custom_token_finder_for(EmbeddedDocument)
+    document.embedded_documents.find_by_token("5678").should == embedded_document
+  end
+
+  it "retrieve a document using the `find` method" do
+    class Document; include Mongoid::Document; field :token; embeds_many :embedded_documents; end
+    class EmbeddedDocument; include Mongoid::Document; field :token; embedded_in :document; end
+
+    document = Document.create!(:token => "1234")
+    embedded_document = EmbeddedDocument.create(token: "5678", document: document)
+
+    Mongoid::Token::Finders.define_custom_token_finder_for(EmbeddedDocument)
+    document.embedded_documents.find("5678").should == embedded_document
   end
 end
